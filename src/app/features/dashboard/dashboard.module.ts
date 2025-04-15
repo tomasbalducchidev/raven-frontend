f46597764ceeb9f3ce339c075ae198aa1e8cacc8
Tomas Balducchi <tomasbalducchi@gmail.com>
2025-04-15 14:52:55 -0300

    wip: create dashboard components

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-
+import { WidgetComponent } from './components/widget/widget.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    DashboardComponent
-  ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+  declarations: [DashboardComponent, WidgetComponent, ChartComponent],
+  imports: [CommonModule, DashboardRoutingModule, SharedModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
