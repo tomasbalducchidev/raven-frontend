fb0289c007f99396a48dc22a3d05b1b12ba8a68d
Tomas Balducchi <tomasbalducchi@gmail.com>
2025-04-15 14:30:11 -0300

    use scss variables and bem methodology

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 
-
-
 @NgModule({
   declarations: [
     CardComponent
