7b28f2c77e88b2f032952de5d07bd348bd50a6cf
Tomas Balducchi <tomasbalducchi@gmail.com>
2025-04-16 16:27:04 -0300

    create datalist view

--- src/app/features/datalist/datalist.component.ts
+++ src/app/features/datalist/datalist.component.ts
@@ -6,6 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datalist.component.scss'],
 })
 export class DatalistComponent implements OnInit {
+  columns = [
+    { key: 'flightNumber', label: 'Número de vuelo' },
+    { key: 'airline', label: 'Aerolínea' },
+    { key: 'departure', label: 'Partida' },
+    { key: 'destination', label: 'Destino' },
+    { key: 'status', label: 'Estado' },
+  ];
+
+  rows = [
+    {
+      flightNumber: 'IB2042',
+      airline: 'Iberia',
+      departure: '08:30',
+      destination: 'Santorini',
+      status: 'A tiempo',
+    },
+    {
+      flightNumber: 'AA0824',
+      airline: 'Aerolíneas Argentinas',
+      departure: '09:30',
+      destination: 'Buenos Aires',
+      status: 'A tiempo',
+    },
+    {
+      flightNumber: 'LT3105',
+      airline: 'Lufthansa',
+      departure: '10:30',
+      destination: 'Múnich',
+      status: 'Demorado',
+    },
+    {
+      flightNumber: 'RY1962',
+      airline: 'Ryanair',
+      departure: '11:30',
+      destination: 'Dublin',
+      status: 'Demorado',
+    },
+    {
+      flightNumber: 'BA1957',
+      airline: 'British Airways',
+      departure: '12:30',
+      destination: 'Londres',
+      status: 'A tiempo',
+    },
+    {
+      flightNumber: 'TK8973',
+      airline: 'Turkish Airlines',
+      departure: '13:30',
+      destination: 'Estambul',
+      status: 'Demorado',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/features/datalist/datalist.module.ts
+++ src/app/features/datalist/datalist.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { DatalistRoutingModule } from './datalist-routing.module';
 import { DatalistComponent } from './datalist.component';
-
+import { TableComponent } from './components/table/table.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    DatalistComponent
-  ],
+  declarations: [DatalistComponent, TableComponent],
   imports: [
     CommonModule,
-    DatalistRoutingModule
-  ]
+    DatalistRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+  ],
 })
-export class DatalistModule { }
+export class DatalistModule {}
