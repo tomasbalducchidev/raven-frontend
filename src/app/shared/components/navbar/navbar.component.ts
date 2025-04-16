b3907210578a255b4dd59fcb0d8fed80e8608e2f
Tomas Balducchi <tomasbalducchi@gmail.com>
2025-04-16 16:55:46 -0300

    create navbar component

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'rv-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private _router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  goTo(path: string): void {
+    this._router.navigate([path]);
+  }
+
+  logout() {
+    this._router.navigate(['']);
+  }
+
+  toggleTheme() {
+    console.log('change theme!');
+
+  }
+
+
+
+}
